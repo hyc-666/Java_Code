f7069d9aaec29ce5b410fdf676c3d1ebb54ddd74
hyc_666 <2213671872@qq.com>
2019-12-21 14:06:14 +0800

    基本数据类型

---
+++ TypeData/TypeData.java
@@ -0,0 +1,39 @@
+public class TypeData{
+	public static void main(String[] args){
+		//整型
+		//type
+		byte maxByte=Byte.MAX_VALUE;
+		System.out.println("maxByte:"+maxByte);
+		byte minByte=Byte.MIN_VALUE;
+		System.out.println("minByte:"+minByte);
+		//short
+		short maxShort=Short.MAX_VALUE;
+		System.out.println("maxShort:"+maxShort);
+		short minShort=Short.MIN_VALUE;
+		System.out.println("minShort:"+minShort);
+		//int
+		int maxInt=Integer.MAX_VALUE;
+		System.out.println("maxInt:"+maxInt);
+		int minInt=Integer.MIN_VALUE;
+		System.out.println("minInt:"+minInt);
+		//long
+		long maxLong=Long.MAX_VALUE;
+		System.out.println("maxLong:"+maxLong);
+		long minLong=Long.MIN_VALUE;
+		System.out.println("minLong:"+minLong);
+		//浮点型
+		//float
+		float maxFloat=Float.MAX_VALUE;
+		float minFloat=Float.MIN_VALUE;
+		System.out.println("maxFloat:"+maxFloat);
+		System.out.println("minFloat:"+minFloat);
+		//double
+		double maxDouble=Double.MAX_VALUE;
+		double minDouble=Double.MIN_VALUE;
+		System.out.println("maxDouble:"+maxDouble);
+		System.out.println("minDouble:"+minDouble);
+		
+		
+		
+	}
+}
