554640491125769ecebd3c19b2a944eaaaacccdc
hyc_666 <2213671872@qq.com>
2019-12-30 13:51:30 +0800

    错误更正

--- TestChar/TestChar.java
+++ TestChar/TestChar.java
@@ -1,9 +1,19 @@
 class TestChar{
 	public static void main(String[] args){
-		char test1 = 'A';
-		char test2 = '3';
-		char test3 = test1 + test2;
-		
-		System.out.println(test3);
+		//char temp = 'a';
+		//short x = 20;
+		////char s = temp + x;
+		//char test = 'B';
+		//char s = 65;
+		//System.out.println(s);
+		//byte a = 10;
+		//short b = 20;
+		//int c = 30;
+		//long d = a + b + c;
+		//System.out.println(d);
+		//long a = 20L;//一般在数字后面跟一个L表示这是long型的，同样也可以加小写的l，但是容易跟数字1混淆。
+		//int b = a;//这样写编译直接报错，除非加强制类型转换，
+		boolean isTrue = true;
+		int a = (int)isTrue;
 	}
 }
