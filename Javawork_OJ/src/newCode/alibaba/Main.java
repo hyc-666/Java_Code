13769a81662e037366fb28fd918c94d87fe2b2a3
hyc_666 <2213671872@qq.com>
2020-09-08 23:43:24 +0800

    阿里笔试

---
+++ Javawork_OJ/src/newCode/alibaba/Main.java
@@ -0,0 +1,66 @@
+package newCode.alibaba;
+
+import java.util.Scanner;
+
+/**
+ * 寻找相邻的组合，一个或多个，计算全部组合
+ * @author hyc
+ * @date 2020/9/8
+ */
+public class Main {
+
+    //第一感觉是用环形链表或者环形队列来做。。。。
+    //分析（试图寻找规律。。。。。）：
+    //    n        f(n)
+    //    1         1   1
+    //    2         3   1,2,(1,2)
+    //    3         6    1,2,3,(1,2),(2,3),(3,1)
+    //    4         13  1,2,3,4,(1,2),(2,3),(3,4),(4,1),(1,2,3),(2,3,4),(3,4,1),(4,1,2),(1,2,3,4)
+    //
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //感觉只是时间不够
+        while (scanner.hasNextInt()){
+            int n = scanner.nextInt();
+        }
+    }
+/*    public static int countCow(SingleList head,int n){
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+
+        }
+    }*/
+}
+class Node{
+    int value;
+    Node next;
+
+    public Node(int value) {
+        this.value = value;
+    }
+}
+
+class SingleList{
+    Node head = null;
+
+    public void add(int value){
+        Node node = new Node(value);
+        if (head == null){
+            head = node;
+            return;
+        }
+        head.next = node;
+    }
+
+    public void circle(){
+        if (head == null){
+            return;
+        }
+        //将此单链表变为环形单链表
+        Node node = head;
+        while (node.next != null){
+            node = node.next;
+        }
+        node.next = head;
+    }
+}
---
+++ Javawork_OJ/src/newCode/alibaba/Main2.java
@@ -0,0 +1,30 @@
+package newCode.alibaba;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * 转换密码的对应关系
+ * @author hyc
+ * @date 2020/9/8
+ */
+public class Main2 {
+    public static void main(String[] args) {
+        //题目都看不懂。。。。。
+        //对应的替换关系要自己找吗？
+        //要一个一个去试？
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextInt()){
+            int n = scanner.nextInt();
+            if (n == 0){
+                break;
+            }
+            List<String> stringList = new ArrayList<>(n + 1);
+            for (int i = 0; i <= n; i++) {
+                stringList.add(scanner.next());
+            }
+
+        }
+    }
+}
